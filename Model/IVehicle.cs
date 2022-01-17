c9a3127f1a8ebf7c06cd51021dc3b01505f488eb
G-collab <galiatinvova@gmail.com>
2022-01-17 16:00:26 +0200

    First Vehicle

---
+++ Model/IVehicle.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+
+namespace Model
+{
+    public interface IVehicle
+    {
+        int ID { get; set; }
+        Vector2 Location { get; set; }
+        Vector2 Velocity { get; set; }
+        float MaxSpeed { get; set; }
+    }
+    
+}
---
+++ Model/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+namespace Model
+{
+    public  class Vehicle : IVehicle
+    {
+        public int ID { get; set; }
+        public Vector2 Location { get; set; }
+        public Vector2 Velocity { get; set; }
+        public float MaxSpeed { get; set; }
+    }
+}
